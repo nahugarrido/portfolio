f7d90d436e015e96ec1ac586a31da04c3f5f355e
Nahuel <68355186+nahugarrido@users.noreply.github.com>
2023-06-26 00:02:52 -0300

    start skill section and style in experience item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ExperienceItemComponent } from './experience-item/experience-item.component';
+import { EducationItemComponent } from './education-item/education-item.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutmeComponent,
+    ExperienceComponent,
+    ExperienceItemComponent,
+    EducationItemComponent,
+    SkillsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/experience/experience.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { ExperienceItemComponent } from '../experience-item/experience-item.component';
+
+@Component({
+  selector: 'app-experience',
+  templateUrl: './experience.component.html',
+  styleUrls: ['./experience.component.css'],
+})
+export class ExperienceComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
